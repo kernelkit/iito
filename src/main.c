cb48401b63784fa6e2da89a7c5ba1a30daabdb72
Tobias Waldekranz <tobias@waldekranz.com>
2023-11-13 13:06:40 +0100

    Add minimal set of commandline options
    Just add the usual suspects to read out version information, control
    logging and reading from a custom config file.

--- src/main.c
+++ src/main.c
@@ -1,22 +1,97 @@
+#include <getopt.h>
+
+#define SYSLOG_NAMES
 #include "iito.h"
+
+#define DEFAULT_CONFIG SYSCONFDIR "/iitod.json"
+
+static void usage()
+{
+	fprintf(stderr,
+		"iitod - If Input, Then Output (Daemon)\n"
+		"\n"
+		"Usage:\n"
+		"  iitod [options]\n"
+		"\n"
+		" Monitor input sources, and reflect their state on output sinks.\n"
+		"\n"
+		"Options:\n"
+		"  -d, --debug         In addition to syslog, also log to stderr\n"
+		"  -f, --config=FILE   Use configuration from FILE instead of %s\n"
+		"  -h, --help          Print usage message and exit\n"
+		"  -l, --loglevel=LVL  Log level: none, err, warn, notice*, info, debug\n"
+		"  -v, --version       Print version information\n",
+		DEFAULT_CONFIG);
+}
+
+static const char *sopts = "df:hl:v";
+static struct option lopts[] = {
+	{ "debug",    no_argument,       0, 'd' },
+	{ "config",   required_argument, 0, 'f' },
+	{ "help",     no_argument,       0, 'h' },
+	{ "loglevel", required_argument, 0, 'l' },
+	{ "version",  no_argument,       0, 'v' },
+
+	{ NULL }
+};
+
+int logmask_from_str(const char *str)
+{
+	const CODE *code;
+
+	for (code = prioritynames; code->c_name; code++)
+		if (!strcmp(str, code->c_name))
+			return LOG_UPTO(code->c_val);
+
+	return -1;
+}
 
 int main(int argc, char **argv)
 {
+	int logopt = 0, logmask = LOG_UPTO(LOG_NOTICE);
 	struct ev_loop *loop = ev_default_loop(0);
+	const char *file = DEFAULT_CONFIG;
 	json_t *cfg, *ins, *outs;
 	json_error_t jerr;
-	int err;
+	int err, opt;
 
-	if (argc < 2)
-		return 1;
+	while ((opt = getopt_long(argc, argv, sopts, lopts, NULL)) > 0) {
+		switch (opt) {
+		case 'd':
+			logopt |= LOG_PERROR;
+			break;
+		case 'f':
+			file = optarg;
+			break;
+		case 'h':
+			usage();
+			return 0;
+		case 'l':
+			logmask = logmask_from_str(optarg);
+			if (logmask < 0) {
+				fprintf(stderr, "Invalid loglevel '%s'\n\n", optarg);
+				usage();
+				exit(1);
+			}
+			break;
+		case 'v':
+			puts(PACKAGE_STRING);
+			return 0;
 
-	openlog(NULL, LOG_PERROR, LOG_DAEMON);
-	setlogmask(LOG_UPTO(LOG_DEBUG));
+		default:
+			fprintf(stderr, "Unknown option '%c'\n\n", opt);
+			usage();
+			return 1;
+		}
+	}
 
-	if (!strcmp(argv[1], "-"))
+	openlog(NULL, logopt, LOG_DAEMON);
+	setlogmask(logmask);
+
+	if (!strcmp(file, "-"))
 		cfg = json_loadf(stdin, 0, &jerr);
 	else
-		cfg = json_load_file(argv[1], 0, &jerr);
+		cfg = json_load_file(file, 0, &jerr);
 
 	if (!cfg) {
 		log_cri("Unable to parse config (%s:%d): %s",
